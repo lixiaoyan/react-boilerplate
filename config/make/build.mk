2dc5fec1326cff1517d821088b3533d36d881ba0
IntPtr <lxy.lixiaoyan@gmail.com>
2016-10-05 00:58:55 +0800

    fix bug for Makefile

--- config/make/build.mk
+++ config/make/build.mk
@@ -4,7 +4,7 @@ build: vendor
 serve: vendor
 	$(WEBPACK_DEV_SERVER) --config ./config/webpack/app.js
 
-dist/vendor/vendor.js: package.json vendor.config.json
+dist/vendor/vendor.js: config/webpack/vendor.js package.json vendor.config.json
 	$(WEBPACK) --config ./config/webpack/vendor.js
 
 vendor: dist/vendor/vendor.js
